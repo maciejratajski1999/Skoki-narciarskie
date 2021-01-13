02230f477377cdd3dd2089e33f9cb2538bad0025
Maciej Ratajski <maciejratajski1999@gmail.com>
2021-01-13 08:44:35 +0100

    Budowa skoczni początek objaśnienia

--- equation.py
+++ equation.py
@@ -5,32 +5,35 @@ from jumper import Jumper
 from hill import Hill
 
 malysz = Jumper(55)
-wisla = Hill(100, 27)
+wisla = Hill(100, 25)
 
 def simulate_jump(hill, *jumpers):
 
-    hill_length = np.linspace(0, 2 * hill.length, 1000)
+    hill_length = np.linspace(0, 1.5 * hill.length, 1000)
     hill_curve = [hill.curve(x) for x in hill_length]
 
-    vy0 = 0
+    vy0 = -0.5
     vx0 = hill.velocity
-
-    time = np.linspace(0,10, 10000)
-    delta_time = 10/10000
+    delta_time = 1 / 1000
+    time = np.arange(0,10, delta_time)
 
     jumps = []
 
     for jumper in jumpers:
-        def square_model(v, t):
-            cd = 0.002 / jumper.mass
+        def linear_model(v):
+            cd = 0.1 / jumper.mass
+            return -cd*v[0]*delta_time, (-cd * v[1] - 9.81)*delta_time
+        def square_model(v):
+            cd = 0.02 / jumper.mass
             l = (v[0] ** 2 + v[1] ** 2) ** (1 / 2)
-            return -cd * v[0] * l, cd * v[1] * l - 9.81
+            return -cd * v[0] * l*delta_time, (-cd * v[1] * l - 9.81)*delta_time
 
-        velocity = odeint(square_model, [vx0,vy0],time)
-        VX, VY = [], []
-        for single in velocity:
-            VX.append(single[0])
-            VY.append(single[1])
+        VX, VY = [vx0], [vy0]
+        for t in time:
+            x, y = square_model([VX[-1], VY[-1]])
+            print(x,y)
+            VX.append(VX[-1] + x)
+            VY.append(VY[-1] + y)
         jumper.move(0,hill.height + 3)
         X,Y = [jumper.position[0]], [jumper.position[1]]
 
@@ -60,5 +63,5 @@ for jump in jumps:
 plt.legend(["ląd","40kg","55kg","70kg"])
 plt.axis('equal')
 plt.gca().set_aspect('equal', adjustable='box')
-# plt.title("Gx : Gy = 1:1")
+plt.title("Gx : Gy = 1:1")
 plt.show()
--- hill.py
+++ hill.py
@@ -1,12 +1,14 @@
 class Hill:
 
-    def __init__(self, lenght, velocity):
+    def __init__(self, length, velocity):
         self.velocity = velocity
-        self.length = lenght
-        self.height = self.length*0.55
-        self.modifier = -((1/(3*self.length))*((self.length)**3) - ((self.length)**2))/self.height
+        self.length = length
+        self.height = self.length*(0.6)
+        self.draw_length = self.length*1.2
+        self.modifier = -((1/(3*self.draw_length))*((self.draw_length)**3) - ((self.draw_length)**2))/self.height
 
     def curve(self, x):
-        return ((1/(3*(self.length)))*(x**3) - (x**2))/self.modifier + self.height
+        x= x + self.length*0.2
+        return ((1/(3*(self.draw_length)))*(x**3) - (x**2))/self.modifier + self.height
 
 
