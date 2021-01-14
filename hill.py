047bbb4903147debe51c586bc896c9386b97eb4b
Maciej Ratajski <maciejratajski1999@gmail.com>
2021-01-14 08:43:43 +0100

    dodanie modułu Jumper

--- hill.py
+++ hill.py
@@ -1,12 +1,20 @@
 class Hill:
 
     def __init__(self, length, velocity):
+        '''
+        :param length: horyzontalny dystans od progu do punktu K
+        :param velocity: prędkość wyjścia z progu
+        '''
         self.velocity = velocity
         self.length = length
         self.height = self.length*(0.6)
         self.draw_length = self.length*1.2
-        self.modifier = -((1/(3*self.draw_length))*((self.draw_length)**3) - ((self.draw_length)**2))/self.height
+        self.modifier = -(-(2/3)*((self.draw_length)**2))/self.height
 
     def curve(self, x):
-        x= x + self.draw_length - self.length
+        '''
+        :return: wielomian trzeciego stopnia od x
+        '''
+        # nie chcem zaczynać z x=0, ponieważ oczekujemy że stok pod progiem od razu będzie nachylony
+        x= x + self.length*0.2
         return ((1/(3*(self.draw_length)))*(x**3) - (x**2))/self.modifier + self.height
