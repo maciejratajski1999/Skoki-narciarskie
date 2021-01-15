fcd7b8e76e34c330c1e44e791524df9cc975e52e
Maciej Ratajski <maciejratajski1999@gmail.com>
2021-01-15 13:40:26 +0100

    Dalsze wykresy, kąt wyjścia z progu

--- comparison.py
+++ comparison.py
@@ -2,9 +2,9 @@ from jumper import Jumper
 from hill import Hill
 import numpy as np
 from matplotlib import pyplot as plt
-from math import sin, cos
+from math import sin, cos, pi
 
-class SkiJumpAnimation:
+class CompareJumpers:
 
     def __init__(self, hill):
         self.hill = hill
@@ -19,7 +19,7 @@ class SkiJumpAnimation:
         hill_length = np.arange(0, 1.5 * self.hill.length, 0.1)
         return [self.hill.curve(x) for x in hill_length], hill_length
 
-    def __linear_model(self, v, mass, Cd, g, area=2):
+    def __linear_model(self, v, Cd, g, area=2):
         '''
         :param v: tuple (Vx(i), Vy(i))
         :param Cd: stała oporu powietrza
@@ -27,13 +27,10 @@ class SkiJumpAnimation:
         :param g: przyspieszenie ziemskie g
         :return : tuple (Vx(i+1),Vy(i+1))
         '''
-        l = (v[0] ** 2 + v[1] ** 2) ** (1 / 2)
-        Cd = Cd/mass
-        area = self.__area(area,v)
-        print(area)
+        # l = (v[0] ** 2 + v[1] ** 2) ** (1 / 2)
         return -(Cd*1) * v[0]  * self.delta_time, (-(Cd*area) * v[1]  - g)  * self.delta_time
 
-    def __square_model(self, v, mass, Cd, g, area):
+    def __square_model(self, v, Cd, g, area):
         '''
         :param v: tuple (Vx(i), Vy(i))
         :param Cd: stała oporu powietrza
@@ -42,28 +39,23 @@ class SkiJumpAnimation:
         :return : tuple (Vx(i+1),Vy(i+1))
         '''
         l = (v[0] ** 2 + v[1] ** 2) ** (1 / 2)
-        Cd = Cd/mass
         return -(Cd*area[0]) * v[0]  * l* self.delta_time, (-(Cd*area[1]) * v[1] * l  - g)  * self.delta_time
 
-    # def __area(self, area, v, l):
-    #     return area * ((abs(v[1])) / l)
 
 
     def show(self, jumpers, cd, g, angle):
-        color = 'b'
         for jumper in jumpers:
-            Cd = cd
-            VX, VY = [self.hill.velocity], [-0.05*self.hill.velocity]
+            VX, VY = [self.hill.velocity*cos(-self.hill.angle)], [self.hill.velocity*sin(-self.hill.angle)]
             mass = jumper.mass
+            Cd = cd / mass
             area = jumper.area*sin(angle), jumper.area*cos(angle)
 
             for t in self.time[1:]:
-                x, y = self.__square_model((VX[-1], VY[-1]), mass, Cd, g, area)
-                # print(t)
+                x, y = self.__square_model((VX[-1], VY[-1]), Cd, g, area)
                 VX.append(VX[-1] + x)
                 VY.append(VY[-1] + y)
 
-            jumper.move(0, self.hill.height)
+            jumper.move(0, self.hill.height+1)
             X, Y = [jumper.position[0]], [jumper.position[1]]
             for i in range(1, len(self.time)):
                 jumper.move(VX[i] * self.delta_time, VY[i] * self.delta_time)
@@ -73,20 +65,18 @@ class SkiJumpAnimation:
                 if y <= self.hill.curve(x) or x > self.hill_length[-1]:
                     break
             plt.plot(X,Y)
+            x, y = X[-1], Y[-1]
+            plt.annotate(str(int(x * 2) / 2) + "m", (x, y))
+        masses = [None]+[str(jumper.mass) + "kg" for jumper in jumpers]
+        plt.legend(masses)
         plt.show()
-            # for i in range(0,len(X), 100):
-            #     c = plt.Circle((X[i],Y[i]), 0.6, color=color)
-            #     self.axs.add_artist(c)
-            #     self.cnv.canvas.draw()
-            #     plt.pause(self.delta_time*100)
-            # color = 'r'
+
+wisła = Hill(100,23, 0.1)
+okienko = CompareJumpers(wisła)
+okienko.show((Jumper(45),Jumper(55),Jumper(65)), cd=0.042, g=9.81, angle=pi/64)
 
 
 
 
-
-wisła = Hill(100,23)
-okienko = SkiJumpAnimation(wisła)
-okienko.show((Jumper(45),Jumper(55),Jumper(65)), cd=0.038, g=9.81, angle=0)
 
 
--- hill.py
+++ hill.py
@@ -1,6 +1,6 @@
 class Hill:
 
-    def __init__(self, length, velocity):
+    def __init__(self, length, velocity, angle=0):
         '''
         :param length: horyzontalny dystans od progu do punktu K
         :param velocity: prędkość wyjścia z progu
@@ -10,6 +10,7 @@ class Hill:
         self.height = self.length*(0.6)
         self.draw_length = self.length*1.25
         self.modifier = -(-(2/3)*((self.draw_length)**2))/self.height
+        self.angle = angle
 
     def curve(self, x):
         '''
@@ -18,3 +19,5 @@ class Hill:
         # nie chcem zaczynać z x=0, ponieważ oczekujemy że stok pod progiem od razu będzie nachylony
         x= x + self.length*0.2
         return ((1/(3*(self.draw_length)))*(x**3) - (x**2))/self.modifier + self.height
+        # return (3/(10*(self.draw_length**2)))*(x**3) - (9/(10*self.draw_length))*(x**2) + self.height
+
