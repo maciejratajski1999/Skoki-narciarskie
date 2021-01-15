c98d5935f8ff607761f6568a8cd011d41bb847d5
Maciej Ratajski <maciejratajski1999@gmail.com>
2021-01-15 18:23:45 +0100

    Poprawki, usunięty kod klasy Hill z main

--- hill.py
+++ hill.py
@@ -1,31 +1,34 @@
-import numpy as np
+import numpy
+
+
 class Hill:
 
     def __init__(self, length, velocity, angle=0):
         '''
         :param length: horyzontalny dystans od progu do punktu K
         :param velocity: prędkość wyjścia z progu
+        :param angle: kąt wyjścia z progu w radianach
         '''
         self.velocity = velocity
         self.length = length
-        self.height = self.length*(0.6)
-        self.draw_length = self.length*1.25
-        self.modifier = -(-(2/3)*((self.draw_length)**2))/self.height
+        self.draw_length = self.length * 1.25
+        self.difference = self.draw_length - self.length
+        self.height = self.length * (self.length / 180)
+        self.modifier = ((2 / 3) * ((self.draw_length) ** 2)) / (self.height)
         self.angle = angle
 
     def curve(self, x):
         '''
         :return: wielomian trzeciego stopnia od x
         '''
-        # nie chcem zaczynać z x=0, ponieważ oczekujemy że stok pod progiem od razu będzie nachylony
-        x= x + self.length*0.2
-        return ((1/(3*(self.draw_length)))*(x**3) - (x**2))/self.modifier + self.height
-        # return (3/(10*(self.draw_length**2)))*(x**3) - (9/(10*self.draw_length))*(x**2) + self.height
+        # nie chcemy zaczynać z x=0, ponieważ oczekujemy że stok pod progiem od razu będzie nachylony
+        x = x + self.difference
+        return ((1 / (3 * (self.draw_length))) * (x ** 3) - (x ** 2)) / self.modifier + self.height
 
     def distance(self, a):
-        delta_x = 0.1
-        xs = np.arange(0,a,delta_x)
-        ys = [self.curve(x)*delta_x for x in xs]
-        surface = [((ys[i]-ys[i-1])**2 + delta_x**2)**(1/2) for i in range(1,len(ys))]
+        # przybliża odległość skoku, dla punktu lądowania o x-owej współrzędnej a
+        delta_x = 1
+        xs = numpy.arange(0, a, delta_x)
+        ys = [self.curve(x) * delta_x for x in xs]
+        surface = [((ys[i] - ys[i - 1]) ** 2 + delta_x ** 2) ** (1 / 2) for i in range(1, len(ys))]
         return sum(surface)
-
