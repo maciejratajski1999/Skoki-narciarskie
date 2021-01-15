b3f46a66909bd333382acd66e0abce620827d02d
Maciej Ratajski <maciejratajski1999@gmail.com>
2021-01-15 21:20:33 +0100

    moduł jumper.py

--- jumper.py
+++ jumper.py
@@ -1,8 +1,11 @@
 class Jumper:
 
     def __init__(self, mass):
-        self.mass = mass
-        self.area = 20
+        self.mass = mass # kg
+        # ponieważ przyspieszenie działające na skoczka jest odwrotne proporcjonalnie do jego masy,
+        # możemy nadmiar masy zrekompensować zwiększając jego powierzchnię
+        # niech powierzchnia wynosi 2m^2 dla skoczka ważącego 55kg
+        self.area = 2*(self.mass/55)    # m^2
         self.position = 0,0
 
     def move(self, x, y):
